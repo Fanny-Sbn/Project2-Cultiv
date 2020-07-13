c41e87e0a21f8782f578223fd49bf07decb92492
Setti <setti@leqoqo.com>
2020-07-13 13:37:54 +0200

    add code to retrieve multiple Ids from one single request + all events

--- public/js/api-service.js
+++ public/js/api-service.js
@@ -1,10 +1,38 @@
-const service = axios.create({
-  baseURL: "http://localhost:3000"
-});
+//const service = axios.create({
+//  baseURL: "http://localhost:3000"
+//});
 
-// console.log(service);
-// service is an axios instance
-// you will use it to interact with the backend in AJAX
-// with the get/post/delete/patch/put verbs ....
+let axios = require("axios");
+let fs = require("fs");
+const date = new
 
-export default service;
+// https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&q=(start105220%20OR%20id:85700)
+
+axios
+  .get(
+    "https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&rows=0"
+  )
+  .then((res) => {
+    return res.data.nhits;
+  })
+  .then((nhits) => {
+    axios
+      .get(
+        `https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&rows=${nhits}`
+      )
+      // .then((res) => {
+      //   const { data } = res;
+      //   const { records } = data;
+      //   const myBeautifulData = records.map((row) => {
+      //     const { fields } = row;
+
+      //     const { coordinates, category, description, tags } = fields;
+
+      //     let clean_tags = [];
+      //     try {
+      //       clean_tags = tags.split(";");
+      //     } catch (e) {
+      //       console.log(e);
+      //     }
+
+//export default service;
