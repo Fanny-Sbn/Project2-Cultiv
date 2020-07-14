6443f8a8711eb55794af48d63c0c4cba0bff310b
Setti <setti@leqoqo.com>
2020-07-14 15:34:05 +0200

    more working on filter

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,19 +1,40 @@
 function myFunction() {
+  const res = axios
+    .get(
+      "https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&rows=50"
+    )
+    .then((res) => {
+      let allTitles = [];
+      const events_pre = res.data.records.forEach((event) => {
+        if (
+          (event.fields.address_city == "Paris" &&
+            event.fields.title != undefined) ||
+          event.fields.title != null
+        ) {
+          allTitles.push(event.fields.title);
+          console.log(allTitles);
+        }
+        const listTitles = document.getElementById("titles");
+        listTitles.forEach((e) => {
+          let li = document.createElement("li");
+          li.addEventListener("click");
+          titles.append(li);
+        });
 
- const res = await axios.get("https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&rows=50")
-              .then((res) => {
-                  return res.data.records.map((e)=>e.title);
-              })
-              .catch((err) => {
-                  console.log(err)
-              })
-
-
-    const checkbox = document.getElementById("myCheck");
-    let input = checkbox.querySelector("input");
-    checkbox.addEventListener("change", updateTags);
-
-    let list = [];
-    if (input.checked) list.push(input.dataset.tagId);
-
+        // let input = checkbox.querySelector("input");
+        // checkbox.addEventListener("change", updateTags);
+        // let list = [];
+        // if (input.checked) list.push(input.dataset.tagId)
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
+myFunction();
+
+//   .then((res) => {
+//     let titles = res.data.records.map((e) => e.title);
+//     console.log(titles);
+//     return titles;
+//   })
