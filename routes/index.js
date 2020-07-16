0fbc02afdd3a8e8282ac93ee0edbffc148db3091
Fanny Sabin <fannysabin.fs@gmail.com>
2020-07-16 12:30:41 +0200

    scroller popup

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,27 @@ router.get("/", (req, res) => {
 
 router.get("/evenement/:id", (req, res) => {
   const id = req.params.id;
-  res.render("oneEvent", { js: ["filter"]})
+  axios.get(`https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&q=id=${id}`)
+    .then(function (response) {
+      let data = response.data.records[0].fields;
+      console.log(data);
+      res.render("oneEvent", {data})
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
+
+
+/*   axios.get()
+    .then(res => {
+      var data = res.data.records;
+      console.log(data);
+      res.render("oneEvent", { data })
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}); */
 
 module.exports = router;
